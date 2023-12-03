e6045bd57b650c3a6166c371fe59da0aef721f7b
Duygu Eylül KARATAŞ <96980093+duygueylul@users.noreply.github.com>
2023-12-04 02:40:29 +0300

    Initial commit

---
+++ src/MathsDenk/Islem.java
@@ -0,0 +1,5 @@
+package MathsDenk;
+
+public class Islem {
+
+}
---
+++ src/MathsDenk/Maths.java
@@ -0,0 +1,90 @@
+package MathsDenk;
+import java.io.*;
+
+public class Maths {
+
+	
+	int Katsayimiz, SayiIsagreti, Sayilar, xToplam, yKatsayisi, Sonuc;
+    
+    String Katsayi, Esit;
+   
+    void Coz( String Denklem,String xDegisken) {
+
+            int xDegiskeni = new Integer(xDegisken);
+
+            for (int i = 0; i < Denklem.length(); i++) {
+
+                    if (Character.isDigit(Denklem.charAt(i))) {
+
+                            Sayilar++;
+
+                    }
+                    else if (Denklem.charAt(i) != '+' && Denklem.charAt(i) != '-' && Denklem.charAt(i) != '=') {
+
+                            if (Denklem.charAt(i - Sayilar) == '+')
+                            {
+                                    SayiIsagreti = 1;
+                            }
+                            else if (Denklem.charAt(i - Sayilar) == '-')
+                            {
+                                    SayiIsagreti = -1;
+                            }
+                            else SayiIsagreti = 1;
+
+                            Katsayi = Denklem.substring(i - Sayilar, i);
+                            int Katsayimiz = new Integer(Katsayi);
+                           
+                            if (Denklem.charAt(i) == 'x')
+                            {
+                                    xToplam += SayiIsagreti * Katsayimiz * xDegiskeni;
+                            }
+                            else if(Denklem.charAt(i) == 'y')
+                            {
+                                    yKatsayisi=Katsayimiz;
+                            }
+                           
+                            Sayilar = 0;
+                    }
+                    else if (Denklem.charAt(i) == '=') {
+
+                           Esit = Denklem.substring(i + 1, Denklem.length());
+                           break;
+
+                    }
+                    else  {
+                            Sayilar = 0;
+                    }
+            }
+
+            int Esittir = new Integer(Esit);
+            Sonuc = (Esittir - xToplam) / yKatsayisi;
+
+            System.out.print("y= " + Sonuc);
+    }
+	
+	
+	
+	
+	
+	public static void main(String[] args) throws IOException {
+		
+		
+		
+		
+		   String Denklem, xDegisken;
+
+           BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
+
+           System.out.print("Denklemi giriniz: ");
+           Denklem = b.readLine();
+          
+           System.out.print("x degisken degerini girin: ");
+           xDegisken = b.readLine();
+
+           Maths DenklemCozucu = new Maths();
+
+           DenklemCozucu.Coz(Denklem, xDegisken);
+
+	}
+
+}
